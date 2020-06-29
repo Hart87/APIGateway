967cd948eba6672ef7eaa80e6e3967493f185d64
James Hart <Hart87@gmail.com>
2020-06-29 11:12:37 -0400

    RR Controller for all REST

--- src/main/java/com/gateway/loadbalancer/RounRobin.java
+++ src/main/java/com/gateway/loadbalancer/RounRobin.java
@@ -3,9 +3,12 @@ package com.gateway.loadbalancer;
 import com.gateway.core.Helper;
 import com.gateway.instance.Instance;
 import com.gateway.instance.InstanceRepository;
+import okhttp3.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,6 +24,7 @@ public class RounRobin {
     // https://tomcat.apache.org/tomcat-5.5-doc/servletapi/javax/servlet/http/HttpServletRequest.html
 
     private static final Logger logger = LoggerFactory.getLogger(RounRobin.class);
+    MediaType JSON = MediaType.parse("application/json; charset=utf-8");
     Helper helper = new Helper();
 
     private final InstanceRepository instances;
@@ -30,13 +34,9 @@ public class RounRobin {
         this.routers = routers;
     }
 
-    //Helper method
-    public void DetermineInstance () {
-        //use this to refactor the code below....
-    }
 
-    @RequestMapping(value = "gateway/v1/rr/**", method = RequestMethod.GET, produces = "application/json")
-    public void RRGet(
+    @RequestMapping(value = "gateway/v1/rr/", method = RequestMethod.GET, produces = "application/json")
+    public String RRGet(
             HttpServletResponse httpServletResponse,
             HttpServletRequest httpServletRequest) throws IOException {
         logger.info("G E T");
@@ -75,15 +75,24 @@ public class RounRobin {
         String theRoute = httpServletRequest.getHeader("route");
         String aString = "https://" + instance.getAddress() + "/" + theRoute  ;
         logger.info(aString);
-        httpServletResponse.setStatus(307);
-        httpServletResponse.addHeader("Authorization", httpServletRequest.getHeader("Authorization"));
-        httpServletResponse.sendRedirect(aString);
+
+        String authHeader = httpServletRequest.getHeader("Authorization");
+        logger.info(authHeader);
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(aString)
+                .addHeader("Authorization", authHeader)
+                .build();
+        Response response = client.newCall(request).execute();
+
+        return response.body().string();
 
     }
 
 
-    @RequestMapping(value = "gateway/v1/rr/**", method = RequestMethod.POST, produces = "application/json")
-    public void RRPost(
+    @RequestMapping(value = "gateway/v1/rr/", method = RequestMethod.POST, produces = "application/json")
+    public String RRPost(
             HttpServletResponse httpServletResponse,
             HttpServletRequest httpServletRequest) throws IOException {
         logger.info("P O S T ");
@@ -120,15 +129,142 @@ public class RounRobin {
         logger.info(httpServletRequest.getQueryString());
         //use this query string to send new http requests that would return the results.
 
-        //NO REDIRECT FOR POST.... THIS IS ONLY HERE FOR GUIDANCE RIGHT NOW
-        //Redirect the to the URL if all is well
         String theRoute = httpServletRequest.getHeader("route");
-        String aString = "https://" + instance.getAddress() + "/" + theRoute  ;
+        String theQuery = httpServletRequest.getQueryString();
+        String aString = "https://" + instance.getAddress() + "/" + theRoute + "/?" + theQuery  ;
         logger.info(aString);
 
-        httpServletResponse.addHeader("Authorization",
-                httpServletRequest.getHeader("Authorization"));
-        httpServletResponse.sendRedirect(aString);
+        //Send it and return the output
+        OkHttpClient client = new OkHttpClient();
+        RequestBody formBody = new FormBody.Builder().build(); //.add for parameters but query string has them
+        Request request = new Request.Builder()
+                .addHeader("Authorization", httpServletRequest.getHeader("Authorization"))
+                .url(aString)
+                .post(formBody)
+                .build();
+        Response response = client
+                .newCall(request)
+                .execute();
+
+        return response.body().string();
+
+    }
+
+    @RequestMapping(value = "gateway/v1/rr/", method = RequestMethod.PUT, produces = "application/json")
+    public String RRPut(
+            HttpServletResponse httpServletResponse,
+            HttpServletRequest httpServletRequest) throws IOException {
+        logger.info("P U T ");
+        long count = 0;
+        //Check if IP address is okay (later)
+
+        //Get router information and determine which instance to send to
+        Router router = routers.findBycallSign("roundrobin");
+        logger.info(router.toString());  //for debug
+
+
+        //In the for and if block first determine how many instances there are.
+        //Then determine what the rotation number will be based on how many instances there are.
+        //The rotation number determines which instance of the server to route to.
+        for (Instance anInstance : instances.findAll()) {
+            count++;
+            logger.info("ROUTE : " + anInstance.getAddress());
+        }
+
+        if (router.rotation >= count) {
+            router.rotation = 1;
+        } else {
+            router.rotation++;
+        }
+
+        //Find and use Instance
+        Instance instance = instances.findByid(router.rotation);
+        logger.info(instance.toString());
+
+        //Update router info
+        routers.save(router); //setting the rotation number for the next iteration
+        //logger.info(router.toString());  //for debug
+
+        logger.info(httpServletRequest.getQueryString());
+        //use this query string to send new http requests that would return the results.
+
+        String theRoute = httpServletRequest.getHeader("route");
+        String theQuery = httpServletRequest.getQueryString();
+        String aString = "https://" + instance.getAddress() + "/" + theRoute + "/?" + theQuery  ;
+        logger.info(aString);
+
+        //Send it and return the output
+        OkHttpClient client = new OkHttpClient();
+        RequestBody formBody = new FormBody.Builder().build(); //.add for parameters but query string has them
+        Request request = new Request.Builder()
+                .addHeader("Authorization", httpServletRequest.getHeader("Authorization"))
+                .url(aString)
+                .put(formBody)
+                .build();
+        Response response = client
+                .newCall(request)
+                .execute();
+
+        return response.body().string();
+
+    }
+
+    @RequestMapping(value = "gateway/v1/rr/", method = RequestMethod.DELETE, produces = "application/json")
+    public String RRDelete(
+            HttpServletResponse httpServletResponse,
+            HttpServletRequest httpServletRequest) throws IOException {
+        logger.info("D E L E T E ");
+        long count = 0;
+        //Check if IP address is okay (later)
+
+        //Get router information and determine which instance to send to
+        Router router = routers.findBycallSign("roundrobin");
+        logger.info(router.toString());  //for debug
+
+
+        //In the for and if block first determine how many instances there are.
+        //Then determine what the rotation number will be based on how many instances there are.
+        //The rotation number determines which instance of the server to route to.
+        for (Instance anInstance : instances.findAll()) {
+            count++;
+            logger.info("ROUTE : " + anInstance.getAddress());
+        }
+
+        if (router.rotation >= count) {
+            router.rotation = 1;
+        } else {
+            router.rotation++;
+        }
+
+        //Find and use Instance
+        Instance instance = instances.findByid(router.rotation);
+        logger.info(instance.toString());
+
+        //Update router info
+        routers.save(router); //setting the rotation number for the next iteration
+        //logger.info(router.toString());  //for debug
+
+        logger.info(httpServletRequest.getQueryString());
+        //use this query string to send new http requests that would return the results.
+
+        String theRoute = httpServletRequest.getHeader("route");
+        String theQuery = httpServletRequest.getQueryString();
+        String aString = "https://" + instance.getAddress() + "/" + theRoute + "/?" + theQuery  ;
+        logger.info(aString);
+
+        //Send it and return the output
+        OkHttpClient client = new OkHttpClient();
+        RequestBody formBody = new FormBody.Builder().build(); //.add for parameters but query string has them
+        Request request = new Request.Builder()
+                .addHeader("Authorization", httpServletRequest.getHeader("Authorization"))
+                .url(aString)
+                .delete(formBody)
+                .build();
+        Response response = client
+                .newCall(request)
+                .execute();
+
+        return response.body().string();
 
     }
 
